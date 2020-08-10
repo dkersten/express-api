72af2f10d49104bdddc2f3527aa795b52bb9b11c
dkersten <dkersten1994@gmail.com>
2020-08-09 20:46:31 -0400

    modified seedd data structure

--- models/cars.js
+++ models/cars.js
@@ -1,16 +1,31 @@
-let cars = {
-    1: {
+let cars = [
+    {
         id: '1',
         name: '811',
         manufacturer: 'Pfister',
         userId: '2'
     },
-    2: {
+    {
         id: '2',
         name: 'Krieger',
         manufacturer: 'Benefactor',
         userId: '1'
+    },
+    {
+        id: '3',
+        name: 'Adder',
+        manufacturer: 'Truffade'
+    },
+    {
+        id: '4',
+        name: 'Autarch',
+        manufacturer: 'Overflod'
+    },
+    {
+        id: '5',
+        name: 'Banshee 900R',
+        manufacturer: 'Bravado'
     }
-}
+]
 
 export default cars
